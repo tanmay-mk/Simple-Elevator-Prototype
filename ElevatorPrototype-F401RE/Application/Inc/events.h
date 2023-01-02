66e0cfbf60d407ec323710c8df0da69005ac00d2
Tanmay Mahendra Kothale <tanmaymkothale@gmail.com>
2023-01-02 20:37:39 +0530

    events added

---
+++ ElevatorPrototype-F401RE/Application/Inc/events.h
@@ -0,0 +1,38 @@
+#ifndef _EVENTS_H_
+#define _EVENTS_H_
+
+/*INDICATES WHICH FLOOR THE ELEVATOR IS CURRENTLY ON*/
+typedef enum STATES
+{
+	GROUND_FLOOR,
+	FIRST_FLOOR,
+	SECOND_FLOOR,
+	THIRD_FLOOR,
+	FOURTH_FLOOR,
+	ROOF
+}states_t;
+
+/*INDICATES ALL THE POSSIBLE ACTIONS THAT CAN HAPPEN DURING THE EXECUTION*/
+typedef enum EVENTS
+{
+	GroundFloorUp = 0, 	/*up button was pressed on ground floor*/
+	FirstFloorUp,		/*up button was pressed on first floor*/
+	FirstFloorDown,		/*down button was pressed on first floor*/
+	SecondFloorUp,		/*up button was pressed on second floor*/
+	SecondFloorDown,	/*down button was pressed on second floor*/
+	ThirdFloorUp,		/*up button was pressed on third floor*/
+	ThirdFloorDown,		/*down button was pressed on third floor*/
+	FourthFloorUp,		/*up button was pressed on fourth floor*/
+	FourthFloorDown,	/*down button was pressed on fourth floor*/
+	RoofDown,			/*down button was pressed on roof floor*/
+	ElevatorGround,		/*ground floor was selected inside elevator*/
+	ElevatorFirst,		/*first floor was selected inside elevator*/
+	ElevatorSecond,		/*second floor was selected inside elevator*/
+	ElevatorThird,		/*third floor was selected inside elevator*/
+	ElevatorFourth,		/*fourth floor was selected inside elevator*/
+	ElevatorRoof,		/*roof floor was selected inside elevator*/
+	ElevatorOpen,		/*elevator open button was pressed inside elevator*/
+	ElevatorClose,		/*elevator close button was pressed inside elevator*/
+}events_t;
+
+#endif /*_EVENTS_H_*/
